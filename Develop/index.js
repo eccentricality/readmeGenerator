fa0a017d9edcc1df820f5494d5adc4cc6fd93287
eccentricality <youngmin13@gmail.com>
2021-06-23 09:35:19 -0400

    generated short array of questions to ask user

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,57 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: 'projectTitle',
+        // check if input field is empty else ask user to provide a proper title
+        validate: (value) => { 
+            if (value) {
+                return true
+            }
+            else {
+                return 'Please input a proper title!'
+            }}
+    },
+    {
+        type: 'input',
+        message: 'Describe your application.',
+        name: 'projectDescription',
+        validate: (value) => { 
+            if (value) {
+                return true
+            }
+            else {
+                return 'Please write a short description!'
+            }}
+    },
+    {
+        type: 'input',
+        message: 'What are some lessons you learned?',
+        name: 'projectLessons',
+        validate: (value) => { 
+            if (value) {
+                return true
+            }
+            else {
+                return 'Please list some lessons learned!'
+            }}
+    },
+    {
+        type: 'input',
+        message: 'What is the deployment link?',
+        name: 'projectDescription',
+        validate: (value) => { 
+            if (value) {
+                return true
+            }
+            else {
+                return 'Please link your deployment here!'
+            }}
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
